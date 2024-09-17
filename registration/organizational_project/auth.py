505c5740b2a7f6cc10a7402000778fa0cd85b209
kehman18 <kehindeadekola96@gmail.com>
2024-09-17 16:21:12 +0100

    added some more functionalities

--- registration/organizational_project/auth.py
+++ registration/organizational_project/auth.py
@@ -5,8 +5,15 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
+    '''responsible for user login'''
     return "<p>login successful</p>"
 
 @auth.route('/logout')
 def logout():
+    '''responsible for user logout'''
     return "<p>logout successful</p>"
+
+@auth.route('/sign-up')
+def sign_up():
+    '''this function is responsible for user sign-up'''
+    return "<p>sign up successful</p>"
--- registration/organizational_project/views.py
+++ registration/organizational_project/views.py
@@ -1,9 +1,9 @@
 '''helps break your code to various smaller portions'''
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
     '''this is the home route'''
-    return '<h1>Test<h1>'
+    return render_template('home.html')
