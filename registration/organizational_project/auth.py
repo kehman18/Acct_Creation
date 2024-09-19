c9ba44ffbec960be850d38d0c230f73bec625b81
kehman18 <kehindeadekola96@gmail.com>
2024-09-19 23:10:18 +0100

    added the input box and backend functionalities

--- registration/organizational_project/auth.py
+++ registration/organizational_project/auth.py
@@ -1,11 +1,13 @@
 '''helps break your code to various smaller portions'''
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     '''responsible for user login'''
+    data = request.form
+    print(data)
     return render_template('login.html')
 
 @auth.route('/logout')
@@ -13,7 +15,7 @@ def logout():
     '''responsible for user logout'''
     return "<p>logout successful</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     '''this function is responsible for user sign-up'''
     return render_template('sign_up.html')
