24151934eec0b09e7f35b5ac829478df5c7293e5
kehman18 <kehindeadekola96@gmail.com>
2024-09-22 20:02:41 +0100

    added a flash message

--- registration/organizational_project/auth.py
+++ registration/organizational_project/auth.py
@@ -18,18 +18,21 @@ def logout():
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     '''this function is responsible for user sign-up'''
-    email = request.form.get('email')
-    FirstName  = request.form.get('FirstName')
-    password1 = request.form.get('password1')
-    password2 = request.form.get('password2')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        FirstName  = request.form.get('FirstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
 
-    if len(email) < 2:
-        #if email address does not contain either @gmail.con or @yahoo.com
-        flash('Email must be greater than 4 characters.')
-        pass
-    if len(FirstName) < 2:
-        pass
-    if len(password1) == len(password2):
-        pass
-    
+        if len(email) < 2:
+            flash('Email must be greater than 4 characters.', category='error')
+        elif len(FirstName) < 2:
+            flash('firstname must be greater than 2 characters.', category='error')
+        elif len(password1) < 7:
+            flash('password must be greater than 6 characters.', category='error')
+        elif len(password1) == len(password2):
+            flash('password do not match', category='error')
+        else:
+            flash('Account Created Successfully', category='success')
+        
     return render_template('sign_up.html')
