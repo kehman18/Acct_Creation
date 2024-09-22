9dd70b17a99e6938de9d1c6166de3fd09e17eaee
kehman18 <kehindeadekola96@gmail.com>
2024-09-22 22:57:49 +0100

    created the database

--- registration/organizational_project/__init__.py
+++ registration/organizational_project/__init__.py
@@ -1,10 +1,16 @@
 '''this is a flask app for my server'''
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = 'database.db'
 
 def create_app():
     '''this is the function that should be able to be called from main.py'''
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'love'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
     
     from .auth import auth
     from .views import views
