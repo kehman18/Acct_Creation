c8deea9fa4943d0e84fd25749ede0f49e04d7687
kehman18 <kehindeadekola96@gmail.com>
2024-09-24 15:32:08 +0100

    fixed the delete button

--- registration/organizational_project/views.py
+++ registration/organizational_project/views.py
@@ -1,5 +1,6 @@
 '''helps break your code to various smaller portions'''
-from flask import Blueprint, render_template, flash, request
+import json
+from flask import Blueprint, render_template, flash, request, jsonify
 from flask_login import login_required, current_user
 from .models import Note
 from . import db
@@ -21,3 +22,15 @@ def home():
             flash('Note has been added', category='success')
 
     return render_template('home.html', user=current_user)
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+    
+    return jsonify({})
