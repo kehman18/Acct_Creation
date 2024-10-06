02d5a00dd75bd98dfa118acd5fb6c06e5e6a3d40
kehman18 <kehindeadekola96@gmail.com>
2024-10-07 00:58:04 +0100

    removed main.py

--- registration/organizational_project/main1.py
+++ registration/organizational_project/main1.py
@@ -1,9 +1,10 @@
-'''modules needed for the python main file'''
+'''
+modules needed for the python main file
 from organizational_project import create_app
 
 app = create_app()
 
-'''
+
 if __name__ == '__main__':
     app.run(debug=True)
 '''
